b99d21d441aeccea6e4aa74e76975416b70b42e6
nphau <tieuhauvn@gmail.com>
2021-11-18 16:09:48 +0700

    feat: add LoopSquares, PrintPowersOfN

---
+++ Chapter2/LoopSquares.java
@@ -0,0 +1,17 @@
+package Chapter2;
+/**
+ * Write a for loop that produces the following output:
+ * 1 4 9 16 25 36 49 64 81 100 
+ * For added challenge, try to modify your code so that it does not need to use the * multiplication operator.
+ * (It can be done! Hint: Look at the differences between adjacent numbers.)
+ */
+public class LoopSquares {
+
+    // https://practiceit.cs.washington.edu/problem/view/bjp4/chapter2/e2-loopSquares
+    public static void loopSquares(int n){
+        for (int i = 0; i <= n; i++){
+            System.out.print((int)Math.pow(i, 2) + " ");
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Java.BJP4.Practiceit.CS
 https://practiceit.cs.washington.edu/problem/list
+
+[![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#table-of-contents)
+
+## 👇 Authors
+<p>
+    <a href="https://nphau.medium.com/" target="_blank">
+    <img src="https://avatars2.githubusercontent.com/u/13111806?s=400&u=f09b6160dbbe2b7eeae0aeb0ab4efac0caad57d7&v=4" width="96" height="96">
+    </a>
+</p>
